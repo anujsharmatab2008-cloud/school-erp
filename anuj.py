5faa1716b0016c2b49080a211c10bd6b8b2b52ca
anujsharmatab2008-cloud <anujsharmatab2008@gmail.com>
2026-05-28 19:27:23 +0530

    Update anuj.py

--- anuj.py
+++ anuj.py
@@ -2,7 +2,9 @@
 # FULL SCHOOL ERP SYSTEM
 # =========================================================
 
-import syimport os
+import sys
+import  os
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "school_erp.db")s
